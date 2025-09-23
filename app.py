e18bb26b5fa2b617f52106352808b3bbebbd4950
Kaynan Mwangi <kaynanmwangi6@gmail.com>
2025-09-23 11:13:18 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from models import db, Member, Book
+from werkzeug.security import generate_password_hash, check_password_hash
+import os
+from datetime import datetime
+from flask_migrate import Migrate
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bookmanager.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+migrate = Migrate(app, db)
+CORS(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bookmanager.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
---
+++ models.py
@@ -0,0 +1,20 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Member(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+
+    def __repr__(self):
+        return f'<Member {self.name}>'
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    author = db.Column(db.String(100), nullable=False)
+    published_date = db.Column(db.Date, nullable=True)
+    def __repr__(self):
+        return f'<Book {self.title} by {self.author}>'
