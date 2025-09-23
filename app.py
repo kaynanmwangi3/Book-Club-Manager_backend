73fa9081b155e2a269b14f4d7eeb45996f1047aa
Kaynan Mwangi <kaynanmwangi6@gmail.com>
2025-09-23 13:23:20 +0300

    updates app.py, models.py and folders

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from models import db, Member, Book
+from models import db, User, Book, Club 
 from werkzeug.security import generate_password_hash, check_password_hash
 import os
 from datetime import datetime
@@ -14,5 +14,3 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 migrate = Migrate(app, db)
 CORS(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bookmanager.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
